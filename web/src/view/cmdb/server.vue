41f75d64b51693eb3d05a5af3a2e21897fb900ec
telexy324 <gis324@sina.com>
2023-02-15 17:57:56 +0800

    2023021504

--- web/src/view/cmdb/server.vue
+++ web/src/view/cmdb/server.vue
@@ -164,6 +164,10 @@ import { Terminal } from 'xterm'
 import { FitAddon } from 'xterm-addon-fit'
 import { ref, onMounted } from 'vue'
 
+let terminalBox = ref(null)
+let term
+let socket
+
 export default {
   name: 'Server',
   components: {
@@ -410,6 +414,60 @@ export default {
     downloadExcelTemplate() {
       downloadTemplate('ExcelTemplate.xlsx')
     },
+    runTerminal() {
+      term = new Terminal({
+        rendererType: 'canvas',
+        cursorBlink: true,
+        cursorStyle: "bar"
+      })
+    const fitAddon = new FitAddon()
+  term.loadAddon(fitAddon)
+  term.open(terminalBox.value)
+  fitAddon.fit()
+
+  // 创建socket连接
+  term.write('正在连接...\r\n');
+  socket = new WebSocket("ws://" + location.hostname +  ":5555/ssh")
+
+  socket.binaryType = "arraybuffer";
+
+  // 打开socket监听事件的方法
+  socket.onopen = function () {
+    term.write('连接成功...\r\n');
+    fitAddon.fit()
+    term.onData(function (data) {
+      // socket.send(JSON.stringify({ type: "stdin", data: data }))
+      // console.log(data)
+      socket.send(data)
+      // console.log(data)
+    });
+    // ElMessage.success("会话成功连接！")
+    var jsonStr = `{"username":"${username.value}", "ipaddress":"${ipaddress.value}", "port":${port.value}, "password":"${password.value}"}`
+    var datMsg = window.btoa(jsonStr)
+    // socket.send(JSON.stringify({ ip: ip.value, name: name.value, password: password.value }))
+    socket.send(datMsg)
+  }
+  socket.onclose = function () {
+    term.writeln('连接关闭');
+  }
+  socket.onerror = function (err) {
+    // console.log(err)
+    term.writeln('读取数据异常：', err);
+  }
+  // 接收数据
+  socket.onmessage = function (recv) {
+    try {
+      term.write(recv.data)
+    } catch (e) {
+      console.log('unsupport data', recv.data)
+    }
+  }
+
+  window.addEventListener("resize", () => {
+    fitAddon.fit()
+  }, false)
+
+},
   }
 }
 </script>
