54d38c72fcc8ad6593f9ab8dfe10f066d3474333
telexy324 <gis324@sina.com>
2023-08-04 19:20:23 +0800

    2023080401

--- server/core/viper.go
+++ server/core/viper.go
@@ -3,35 +3,44 @@ package core
 import (
 	"flag"
 	"fmt"
+	"github.com/flipped-aurora/gin-vue-admin/server/utils"
 	"os"
 	"path/filepath"
+	"runtime/debug"
 	"time"
 
 	"github.com/songzhibin97/gkit/cache/local_cache"
 
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	_ "github.com/flipped-aurora/gin-vue-admin/server/packfile"
-	"github.com/flipped-aurora/gin-vue-admin/server/utils"
-
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
 
 func Viper(path ...string) *viper.Viper {
 	var config string
+	info, _ := debug.ReadBuildInfo()
 	if len(path) == 0 {
 		flag.StringVar(&config, "c", "", "choose config file.")
+		version := flag.Bool("v", false, "building info.")
 		flag.Parse()
-		if config == "" { // 优先级: 命令行 > 环境变量 > 默认值
-			if configEnv := os.Getenv(utils.ConfigEnv); configEnv == "" {
-				config = utils.ConfigFile
-				fmt.Printf("您正在使用config的默认值,config的路径为%v\n", utils.ConfigFile)
-			} else {
-				config = configEnv
-				fmt.Printf("您正在使用GVA_CONFIG环境变量,config的路径为%v\n", config)
+		if *version {
+			for _, setting := range info.Settings {
+				fmt.Println(setting)
 			}
+			return nil
 		} else {
-			fmt.Printf("您正在使用命令行的-c参数传递的值,config的路径为%v\n", config)
+			if config == "" { // 优先级: 命令行 > 环境变量 > 默认值
+				if configEnv := os.Getenv(utils.ConfigEnv); configEnv == "" {
+					config = utils.ConfigFile
+					fmt.Printf("您正在使用config的默认值,config的路径为%v\n", utils.ConfigFile)
+				} else {
+					config = configEnv
+					fmt.Printf("您正在使用GVA_CONFIG环境变量,config的路径为%v\n", config)
+				}
+			} else {
+				fmt.Printf("您正在使用命令行的-c参数传递的值,config的路径为%v\n", config)
+			}
 		}
 	} else {
 		config = path[0]
--- server/main.go
+++ server/main.go
@@ -19,7 +19,10 @@ import (
 // @name x-token
 // @BasePath /
 func main() {
-	global.GVA_VP = core.Viper()      // 初始化Viper
+	global.GVA_VP = core.Viper() // 初始化Viper
+	if global.GVA_VP == nil {
+		return
+	}
 	global.GVA_LOG = core.Zap()       // 初始化zap日志库
 	global.GVA_DB = initialize.Gorm() // gorm连接数据库
 	initialize.Timer()
